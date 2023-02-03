1ad33cf1b98d94580bede8dc0d05f8569fc6c668
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2023-02-02 20:10:18 -0800

    started navBar

---
+++ src/Logout/BottomNavBar.jsx
@@ -0,0 +1,12 @@
+import './BottomNavBar.css'
+
+function BottomNavBar() {
+
+ return (
+  <div className="bottomNavBarContainer">
+   
+  </div>
+ )
+}
+
+export default BottomNavBar;
---
+++ src/Logout/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+function NavBar() {
+
+ return(
+  <div className="navBarContainer">
+   <div className="navBar">
+    <h1 className="navBarTitle">LEAGUE</h1>
+   </div>
+  </div>
+ )
+}
+
+export default NavBar
